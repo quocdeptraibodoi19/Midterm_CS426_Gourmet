60422cb14a01a3cdfa817a5062281e61f68c77fe
hoai1107 <khaihoai1107@gmail.com>
2022-06-30 22:47:40 +0700

    Add map fragment

---
+++ app/src/main/java/com/example/gourmet/Activity_Fragment/MapsFragment.java
@@ -0,0 +1,93 @@
+package com.example.gourmet.Activity_Fragment;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.example.gourmet.R;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsFragment extends Fragment {
+    private GoogleMap mMap;
+    private Spinner spinner;
+
+    // Test data
+    private String[] names = {"Home", "School", "Bookstore"};
+    private double lat[] = {10.8111573, 10.762902459902067, 10.774353705462936};
+    private double lng[] = {106.67847448373712,106.68208876402593, 106.70450299113612};
+
+    private OnMapReadyCallback callback = new OnMapReadyCallback() {
+
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            mMap = googleMap;
+            setMapPosition(0);
+        }
+    };
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_maps, container, false);
+
+        // Initialize spinner
+        spinner = (Spinner) view.findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                setMapPosition(i);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this.getActivity(), R.layout.spinner_item, names);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(callback);
+        }
+    }
+
+    private void setMapPosition(int position){
+        LatLng pos = new LatLng(lat[position], lng[position]);
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(pos, 15);
+        mMap.moveCamera(cameraUpdate);
+        Marker marker = mMap.addMarker(new MarkerOptions()
+                .position(pos)
+                .title(names[position])
+                .snippet("Address of this place"));
+
+        assert marker != null;
+        marker.showInfoWindow();
+    }
+}
