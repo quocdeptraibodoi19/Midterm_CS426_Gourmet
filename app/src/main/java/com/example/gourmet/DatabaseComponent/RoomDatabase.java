ee6d872257b32032a948c5dd421acbfa35b933a3
quocdeptraibodoi19 <quocthogminhqtm@gmail.com>
2022-07-02 02:14:15 +0700

    Provide frame to add data

--- app/src/main/java/com/example/gourmet/DatabaseComponent/RoomDatabase.java
+++ app/src/main/java/com/example/gourmet/DatabaseComponent/RoomDatabase.java
@@ -1,16 +1,23 @@
 package com.example.gourmet.DatabaseComponent;
 
 import android.app.Application;
+import android.os.AsyncTask;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.example.gourmet.DataElement.ProductElement;
 import com.example.gourmet.DataElement.StoreElement;
 import com.example.gourmet.DataElement.TransactionDetailElement;
 import com.example.gourmet.DataElement.TransactionElement;
 
-@Database(entities = {ProductElement.class, StoreElement.class, TransactionElement.class, TransactionDetailElement.class},version = 6,exportSchema = true)
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = {ProductElement.class, StoreElement.class, TransactionElement.class, TransactionDetailElement.class},version = 4,exportSchema = false)
 public abstract class RoomDatabase extends androidx.room.RoomDatabase {
     public abstract ProductDao productDao();
     public abstract StoreDao storeDao();
@@ -21,12 +28,35 @@ public abstract class RoomDatabase extends androidx.room.RoomDatabase {
         if(instance == null){
             synchronized (RoomDatabase.class){
                 if(instance == null){
+                    Log.d("Thi", "onCreate: populate database");
                     instance = Room.databaseBuilder(application,RoomDatabase.class,"GourmetRoomDatabase")
-                            .createFromAsset("assets/database/Product.db")
+//                            .addCallback(new Callback() {
+//                                @Override
+//                                public void onCreate(@NonNull SupportSQLiteDatabase db) {
+//                                    super.onCreate(db);
+//                                    Log.d("Thi", "onCreate: populate database");
+//                                    new PopulateDbAsyncTask(instance).execute();
+//                                }
+//                            })
+//                            .createFromAsset("database/Gourmet.db")
+                            .fallbackToDestructiveMigration()
                             .build();
                 }
             }
         }
         return instance;
+    }
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
+        private ProductDao dao;
+        PopulateDbAsyncTask(RoomDatabase instance) {
+            dao = instance.productDao();
+        }
+        @Override
+        protected Void doInBackground(Void... voids) {
+            ProductElement productElement = new ProductElement(1,"Cà chua Đà Lạt","https://bizweb.dktcdn.net/100/021/951/products/tomatoes1-550x.jpg?v=1626769632373",25000,"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit pariatur odio obcaecati iusto veritatis modi est explicabo doloremque veniam omnis aliquam deserunt, laboriosam quos hic repellat ratione vel. Aspernatur, omnis.","vegetable","500gr");
+            Log.d("Thi", "doInBackground: "+productElement.getNameProduct());
+            dao.InsertProductDao(productElement);
+            return null;
+        }
     }
 }
